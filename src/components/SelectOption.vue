f11d7bf2bbf66923808e30a81bf78eb750dc1f94
AkatorMr <marck_sparkle@hotmail.com>
2022-03-30 23:09:41 -0300

    Creando el modulo analizar

---
+++ src/components/SelectOption.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="MuroDeCarga">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MuroDeCarga",
+  props: {
+    bMostrar: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      bIngresoDatos: false,
+    };
+  },
+  methods: {
+    Mostrar() {},
+  },
+};
+</script>
+    
+<style scoped>
+.MuroDeCarga {
+  position: absolute;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  background-color: rgba(0, 102, 128, 0.527);
+  z-index: 99999;
+}
+</style>
