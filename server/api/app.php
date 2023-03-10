76b30e0d78353dd1cab5cb68992ebee28c65e160
AkatorMr <marck_sparkle@hotmail.com>
2023-03-10 05:27:40 -0300

    moved function

--- server/api/app.php
+++ server/api/app.php
@@ -28,10 +28,33 @@ function ListarCodigos()
 }
 
 
+function InsertarOperario($_ARGS)
+{
+    $legajo = $_ARGS["legajo"];
+    $apellido = $_ARGS["apellido"];
+    $nombre = $_ARGS["nombre"];
+    $sector = $_ARGS["sector"];
+
+    if (IN("INSERT INTO operarios (legajo,Nombre,Apellido,Sector)VALUES('$legajo','$nombre','$apellido','$sector')")) {
+        return json_encode("ok");
+
+    }
+
+    return json_encode("error");
+}
+
+
+
 $router->addApiEntry("ListarOperarios", "ListarOperarios");
 $router->addApiEntry("ListarPedidos", "ListarPedidos");
 $router->addApiEntry("ListarCodigos", "ListarCodigos");
+$router->addApiEntry("InsertarOperario", "InsertarOperario");
 
+function Prueba($args){
+    print_r($args);
+}
+
+$router->addApiEntry("Prueba", "Prueba");
 
 
 
