fd7f3a1ca83651ceb279e4a5e29c122a8df3b43e
AkatorMr <marck_sparkle@hotmail.com>
2023-03-10 05:28:00 -0300

    add parse args to function callback

--- server/api/Router.php
+++ server/api/Router.php
@@ -1,37 +1,63 @@
 <?php
 
-class Router{
+class Router
+{
 
     private static $instance = null;
     private $tree = array();
-
-    private function __construct(){
+    private $args = array();
+    private function __construct()
+    {
 
     }
 
-    public static function getInstance(){
+    public static function getInstance()
+    {
 
-        if(self::$instance==null){
+        if (self::$instance == null) {
             self::$instance = new Router();
         }
 
         return self::$instance;
     }
 
-    public function addApiEntry($path,$func){
+    public function addApiEntry($path, $func)
+    {
 
-        array_push($this->tree,array("/api/".$path,$func));
+        array_push($this->tree, array("/api/" . $path, $func));
 
     }
 
-    public function execute($request){
-        for($i =0; $i<count($this->tree);$i++){
+    private function PrepareArgs()
+    {
+        $post = array_keys($_POST);
+        $get = array_keys($_GET);
+
+        $this->args = array_merge($_POST, $_GET);
+        //TODO verificar los valores de los arrays
+        /* foreach($post as $key){
+        $this->args[$key] = $_POST[$key];
+        } */
+    }
+
+    public function execute($requested)
+    {
+        $this->PrepareArgs();
+        $request = $requested;
+
+        if (stripos($requested, "?")!==false) {
+            $t = explode("?", $requested);
+            $query = $t[1];
+            $request = $t[0];
+        }
+
+        for ($i = 0; $i < count($this->tree); $i++) {
             $path = $this->tree[$i][0];
             $func = $this->tree[$i][1];
 
-            if($request==$path){
-                if(function_exists($func)){
-                    return call_user_func($func);
+            if ($request == $path) {
+                if (function_exists($func)) {
+                    return call_user_func($func, $this->args);
                 }
 
                 return json_encode("Error: Función no encontrada");
